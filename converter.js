8819f9c78ac7f89c64bf30d6ffd788f7e9a92ec2
davidfdriscoll <david.f.driscoll@gmail.com>
2021-03-10 14:21:29 -0800

    Revises definition data structure

--- converter.js
+++ converter.js
@@ -5,22 +5,35 @@ var fs = require('fs');
 /* one problem is dealing with html-like use of tags like <gloss>
 see discussion at https://github.com/Leonidas-from-XIV/node-xml2js/issues/283 */
 var parser = new xml2js.Parser({includeWhiteChars: 'true',charsAsChildren: 'true',explicitChildren: 'true',preserveChildrenOrder: 'true'});
-var allowed_tags = '<TEI><text><body><head><div><p><gloss><quote><bibl><term>'
+const allowed_tags = '<TEI><text><body><head><div><p><gloss><quote><bibl><term>'
 
-const superscriptDict = new Map([
-  ['0', '⁰'],
-  ['1', '¹'],
-  ['2', '²'],
-  ['3', '³'],
-  ['4', '⁴'],
-  ['5', '⁵'],
-  ['6', '⁶'],
-  ['7', '⁷'],
-  ['8', '⁸'],
-  ['9', '⁹']
-]);
+var toSuperscript = function(text) {
+    // looks for Greek character, non white space, then number
+    // misses things like μετα- 6, so could be improved
+    const superscriptTest = /[\u0370-\u03ff\u1f00-\u1fff]\S+([0-9])/g;
+    const superscriptDict = new Map([
+      ['0', '⁰'],
+      ['1', '¹'],
+      ['2', '²'],
+      ['3', '³'],
+      ['4', '⁴'],
+      ['5', '⁵'],
+      ['6', '⁶'],
+      ['7', '⁷'],
+      ['8', '⁸'],
+      ['9', '⁹']
+    ]);
+    function wordToSuperscript(str, p1, offset) {
+      return(str.substring(0, str.length - 1) + superscriptDict.get(p1));
+    }
+    return text.replace(superscriptTest, wordToSuperscript);
+}
 
-var handleP = function(para) {
+var pToTextArray = function(para) {
+  // takes a p obj from the JSON and iterates over it, evaluating each of its children.
+  // It creats an object consisting of a textType and text
+  // to the array of a TextArray object with a tag
+
   let newPara = [];
   for(let i = 0; i<para['$$'].length; i++) {
     const textObj = para['$$'][i];
@@ -46,7 +59,7 @@ var handleP = function(para) {
     }
     newPara.push(newTextObj);
   }
-  return newPara;
+  return {type: 'textArray', data: newPara};
 }
 
 var parseWord = function(oldWord) {
@@ -54,66 +67,72 @@ var parseWord = function(oldWord) {
   newWord.headword = oldWord.head[0]['_'];
   newWord.etym = [];
   newWord.forms = [];
-  newWord.defs = [];
+  newWord.defs = {type: 'definition', data: []};
 
   console.log(newWord.headword);
 
-  //likely there's a more elegant way to filter these arrays
+  // This loop goes over the p's that exist before the definition divs; it is possible for 
+  // them to include information on etymology, forms, and definitions, and this loop
+  // makes a half-hearted effort to sort the p's accordingly. It should be revised to preserve order.
   if(Array.isArray(oldWord.p) && oldWord.p.length > 0) {
     oldWord.p.forEach(pItem => {   
       if(Array.isArray(pItem['$$']) && pItem['$$'].length > 0) {
-        let newPItem = handleP(pItem);
-        if (newPItem[0].textType === 'term') {
-          newWord.forms.push(newPItem);  
+        let newTextArray = pToTextArray(pItem);
+        if (newTextArray.data[0].textType === 'term') {
+          newWord.forms.push(newTextArray);  
         }
-        else if (newPItem[0].text[0] === '[' || newPItem[0].text[0] === '-') {
-          newWord.etym.push(newPItem);   
+        else if (newTextArray.data[0].text[0] === '[' || newTextArray.data[0].text[0] === '-' || newTextArray.data[0].text[0] === '(') {
+          newWord.etym.push(newTextArray);   
         }
         else {
-          newWord.defs.push(newPItem);
+          newWord.defs.data.push({type: 'meaning', data:[newTextArray]});
         }
       }      
     });
   }
 
+  // This loop goes over the divs that contain definition information.  
+  
   if(Array.isArray(oldWord.div) && oldWord.div.length > 0) {
     oldWord.div.forEach(divItem => {
-      let newDef = [];
-      if(divItem.head) newDef.push({textType: 'headNumber', text: divItem.head[0]['_']});
+      let newMeaning = {type: 'meaning', data: []};
+      if(divItem.head) {
+        newMeaning['head'] = divItem.head[0]['_'];
+      } 
       if(Array.isArray(divItem.p) && divItem.p.length > 0) {
         divItem.p.forEach(pItem => {
-          newDef.push(handleP(pItem));
+          newMeaning.data.push(pToTextArray(pItem));
         });
       }
-      if(newDef.length > 0) newWord.defs.push(newDef);
+      if(newMeaning.head || newMeaning.data.length > 0) {
+        newWord.defs.data.push(newMeaning);
+      }
     });
   }
+  if (newWord.defs.data.length === 0) delete newWord.defs;
   return newWord;
 }
 
 var convertFile = function(file) {
   fs.readFile('./input/' + file, 'utf8', function(err, data) {
-    // remove extraneous tags like <foreign>
-    var strippedOfTags = strip_tags(data, allowed_tags);
-    console.log("stripped of tags");
+    // remove extraneous tags like <foreign>, & superscript numbers at ends of Greek words
+    data = strip_tags(data, allowed_tags);
+    data = toSuperscript(data);
 
-    const superscriptTest = /[\u0370-\u03ff\u1f00-\u1fff]\S+([0-9])/g;
-    function toSuperscript(str, p1, offset) {
-      console.log(p1);
-      console.log(str.substring(0, str.length - 1) + superscriptDict.get(p1));
-      return(str.substring(0, str.length - 1) + superscriptDict.get(p1));
-    }
-    var superscriptNumbers = strippedOfTags.replace(superscriptTest, toSuperscript);
-    console.log("to superscript");
+    parser.parseStringPromise(data).then(function (result) {
 
-    parser.parseStringPromise(superscriptNumbers).then(function (result) {
+      // Execute conversion
       var oldWords = result.TEI.text[0].body[0].div;
       let words = [];
       for(let oldWord of oldWords) {
         words.push(parseWord(oldWord));
       }
- //   console.log(oldWords);
- //   console.log(words);
+
+      // Log to console
+      console.log(oldWords);
+      console.log(words);
+
+      /// Write to JSON file
       console.log("Done, now writing to file");
       var jsonString = JSON.stringify(words);
       var fileName = file.replace('.xml', '.json');
